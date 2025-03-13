de2c127df32b1468b6a616beb58524b219a570e8
NDI05 <andhikap877@gmail.com>
2025-03-13 11:08:59 +0700

    Fear: add void bubble sort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -3,3 +3,29 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void inputArrayData(){
+
+};
+
+void bubbleSort(){
+    int pass = 1
+    do{
+        (for int j = 0; j <= n-1-pass; j++){
+            if(arr[j] > arr[j+1]){
+                int temp
+                tempt = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }while(pass <= n-1);
+};
+
+void display(){
+
+};
+
+int main(){
+
+};
